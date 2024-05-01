e440da7e956f8f57c3535b6e38d5ad1db43007b9
shoaibvultara <141502134+shoaibvultara@users.noreply.github.com>
2024-05-01 11:58:05 +0500

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,33 @@
+const { defineConfig } = require("cypress");
+const { registerAIOTestsPlugin } = require('cypress-aiotests-reporter/src') // Import the necessary plugin if not already imported
+
+module.exports = defineConfig({
+  env: {
+    baseURL: "http://localhost:4200",
+    apiURL: "http://localhost:4201/api",
+    username: "vultara_automation_test",
+    password: "tJVJhiHmlIWR",
+    aioTests: {
+      enableReporting: true,
+      cloud: {
+        apiKey: "NjI3NjRiZjItNjE5NC0zNWYwLWFkMjAtYzZiNmM2MjMwNmQ4LjQ5N2U4YWQxLTQzNTQtNDE4YS04ZDI2LTcwZTU5NTY0NDFhMQ==",
+      },
+      jiraProjectId: "KAN",
+      cycleDetails: {
+        createNewCycle: true,
+        cycleName: "Automation Run " + new Date().toISOString(),
+        //cycleKey: "MAIN-CY-123",
+      },
+      addNewRun: true,
+      addAttachmentToFailedCases: true,
+      createNewRunForRetries: true,
+    },
+  },
+  //projectId: "6ca4i2",
+  e2e: {
+    setupNodeEvents(on, config) {
+      registerAIOTestsPlugin(on, config);
+      // implement node event listeners here
+    },
+  },
+});
